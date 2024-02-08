7a12410ae6428c15e8c7a0de32a32e9d09883809
masud520 <masud.abdi@student.moringaschool.com>
2024-02-08 10:35:46 +0300

    updated the database

--- app/models.py
+++ app/models.py
@@ -10,7 +10,16 @@ class Author(db.Model, SerializerMixin):
     name = db.Column(db.String(100), nullable=False)
 
     def __repr__(self):
-         return f"Author: {self.name}"
+        return f"Author: {self.name}"
+
+class Category(db.Model, SerializerMixin):
+    __tablename__ = 'categories'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f"Category: {self.name}"
 
 class Quote(db.Model, SerializerMixin):
     __tablename__ = 'quotes'
@@ -18,8 +27,10 @@ class Quote(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(255), nullable=False)
     author_id = db.Column(db.Integer, db.ForeignKey('authors.id'))
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
 
     author = db.relationship('Author', backref='quotes')
+    category = db.relationship('Category', backref='quotes')
 
     def __repr__(self):
-         return f"Text: {self.text}, Author: {self.author.name}"
+        return f"Text: {self.text}, Author: {self.author.name}, Category: {self.category.name}"
